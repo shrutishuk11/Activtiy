4b259f6e83f3f57be0271208a1e54e0564bab31b
shrutishuk11 <124597466+shrutishuk11@users.noreply.github.com>
2023-03-30 04:46:11 +0000

    main

---
+++ Introductory_Exercise3.py
@@ -0,0 +1,10 @@
+# unit of pressure convert into pascal,kilo,torr,bar
+P = int(input("enter a pressure in atm "))
+pascal = P*101325
+kilo = P*10332.27
+torr = P*760
+bar = P*1.01325
+print("pressure in bar=",bar)
+print("pressure in pascal=",pascal)
+print("pressure in kilograms/m^2=",kilo)
+print("pressure in torr=",torr)
