71b1033f6d7f9964ec63937d8a307085a459783c
shrutishuk11 <124597466+shrutishuk11@users.noreply.github.com>
2023-04-02 13:56:29 +0000

    main

---
+++ Exercise_Basic.py
@@ -0,0 +1,34 @@
+class method:
+    
+    def _init_(self, name of medicine, batch number, price):
+        self.setName of medicine(name of medicine)
+        self.setBatch number(batch number)
+        self.setPrice(price)
+    
+    def setNameofmedicine(self, name of medicine):
+        self.__name=name
+    
+    def getNameofmedicine(self):
+        return self.__name of medicine
+    
+    def setBatchnumber(self, batch number):
+        self.__batch number=batch number
+    
+    def getbatchnumber(self):
+        return self.__batch number
+    
+    def setPrice(self, price):
+        self.__price=price
+    
+    def getPrice(self):
+        return self.__price
+
+if _name of medicine_ == "_main_":
+    method = method("Stobal")
+    name of medicine = method.getName of medicine()
+    Batch number = method.getBatch number()
+    price = method.getPrice()
+    
+    print(name of medicine)
+    print(batch number)
+    print(price)
